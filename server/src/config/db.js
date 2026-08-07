b8c35f7c3b133a97ad45de11513d70c60d86ee23
kevin17-kdb <natsuananth@gmail.com>
2026-08-08 02:12:50 +0530

    Configure public DNS resolution in db.js for MongoDB Atlas SRV connection

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,5 +1,12 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import dns from "dns";
+
+try {
+  dns.setServers(["8.8.8.8", "1.1.1.1"]);
+} catch (e) {
+  // ignore
+}
 
 dotenv.config();
 
