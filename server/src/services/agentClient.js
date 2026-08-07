7875be0818640bc98ed3b65600dc44b509f6a7ac
kevin17-kdb <natsuananth@gmail.com>
2026-08-08 02:20:03 +0530

    Use 127.0.0.1 for Python agent service URL to prevent Windows IPv6 connection issues

--- server/src/services/agentClient.js
+++ server/src/services/agentClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const agentClient = axios.create({
-  baseURL: process.env.AGENT_SERVICE_URL || "http://localhost:8100",
+  baseURL: process.env.AGENT_SERVICE_URL || "http://127.0.0.1:8100",
   timeout: 30000,
 });
 
